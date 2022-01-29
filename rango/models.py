655a721ceaf3ce6fa9244e42495e70253921f12c
Dom Jina <2524785j@student.gla.ac.uk>
2022-01-29 23:20:40 +0000

    5.6 done

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
+#from ssl import VERIFY_ALLOW_PROXY_CERTS
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+    
+class Page(models.Model):
+    catagory = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.

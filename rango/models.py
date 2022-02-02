bda2b5b51221203fd8dd39e76f951cd0b7e0010e
Dom Jina <2524785j@student.gla.ac.uk>
2022-02-02 15:12:17 +0000

    up to chapter 5 exercises

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -11,12 +13,12 @@ class Category(models.Model):
         return self.name
     
 class Page(models.Model):
-    catagory = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 # Create your models here.
